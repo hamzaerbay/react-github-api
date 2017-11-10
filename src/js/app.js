cc62c299c79c311bca14912f18e3aaa3e64ac930
Hamza Erbay <hamza@udemy.com>
2017-11-10 00:06:03 -0800

    Initial commit

---
+++ src/js/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import GithubCard from './Github-card';
+import '../sass/style.scss';
+
+ReactDOM.render(<GithubCard />, document.getElementById('root'));
